f3330d9ff3f69d8b64f11ebdf678e79c19dd3222
Anton <antonlukas25@gmail.com>
2020-11-23 09:58:45 +0100

    lavet en smule bygningsadmin

--- src/com/company/bugningsadminstrationssystem.java
+++ src/com/company/bugningsadminstrationssystem.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class bugningsadminstrationssystem {
 
+    Scanner sc = new Scanner(System.in)
+
+
     public static void main(String[] args) {
-	// write your code here
+
     }
+ArrayList buildings = new ArrayList<String>();
+
+    public void addbuilding (){
+
+   Building newbuilding = new Building();
+
+   buildings.add(newbuilding)
+
+
+    }
+
 }
+
+
+
