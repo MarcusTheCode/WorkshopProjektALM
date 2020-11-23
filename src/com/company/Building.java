f6cf2eda07c5a42147bc4207db5e4bab777cfb36
Marcus <Marcusbyllemos@gmail.com>
2020-11-23 09:57:52 +0100

    Added buildins fuctionsality

--- src/com/company/Building.java
+++ src/com/company/Building.java
@@ -1,4 +1,38 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Building {
+    public List<Sensorer> sensorerList;
+    public List<Actuator> actuatorList;
+    public int id;
+
+    Building(int id){
+        this.id = id;
+    }
+
+    public void addTempSensor(Sensorer s){
+        sensorerList.add(s);
+    }
+
+    public void removeTempSensor(Sensorer s){
+        sensorerList.remove(s);
+    }
+
+    public void addCO2Sensor(CO2Sensor c){
+        sensorerList.add(c);
+    }
+
+    public void removeCO2Sensor(CO2Sensor c){
+        sensorerList.remove(c);
+    }
+
+    public void removeActuator(Actuator a){
+        actuatorList.remove(a);
+    }
+
+    public void addActuator(Actuator a){
+        actuatorList.add(a);
+    }
 }
